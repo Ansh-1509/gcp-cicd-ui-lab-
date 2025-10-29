fda0a0e98140de7454d62971c7c29cdf5ed11d00
Ansh Parashar <anshparashar1509@gmail.com>
2025-10-29 12:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD Pipeline using GitHub and GCP UI!"
+return "Deployed via GCP CI/CD UI!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
