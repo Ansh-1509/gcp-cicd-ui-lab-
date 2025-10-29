f6dbe2c28609e308df6975ac7759b127fb6c6341
Ansh Parashar <anshparashar1509@gmail.com>
2025-10-29 12:31:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-return "Deployed via GCP CI/CD UI!"
+def hello():
+    return "Hello from Parth!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+
